72685ea967668f9b9529b24bab425176fcd63de3
Frode Oijord <FOeijord@slb.com>
2020-02-07 10:44:45 +0100

    refactor scene def

--- Viewer/sparse3d/sparse3d.scm
+++ Viewer/sparse3d/sparse3d.scm
@@ -1,5 +1,5 @@
 (begin
-   (define sparse-texture2d (filename)
+   (define sparse-texture (filename)
       (group
          (sampler 
             VK_FILTER_LINEAR
@@ -74,6 +74,11 @@
             (uint32 0)
             VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST)))
 
+   (define slice (z) 
+      (indexed-shape 
+         (bufferdata-uint32 0 1 2 2 3 0)
+         (bufferdata-float 0 0 z  1 0 z  1 1 0.5  0 1 z)))
+
    (define lod-color-attachment 
       (framebuffer-attachment 
          VK_FORMAT_R8G8B8A8_UINT
@@ -92,149 +97,121 @@
             VK_IMAGE_USAGE_SAMPLED_BIT)
          (imageaspectflags VK_IMAGE_ASPECT_COLOR_BIT)))
 
-   (define lod-renderpass (renderpass
-      (renderpass-description
-         (renderpass-attachment
-            VK_FORMAT_R8G8B8A8_UINT
-            VK_SAMPLE_COUNT_1_BIT
-            VK_ATTACHMENT_LOAD_OP_CLEAR
-            VK_ATTACHMENT_STORE_OP_STORE
-            VK_ATTACHMENT_LOAD_OP_DONT_CARE
-            VK_ATTACHMENT_STORE_OP_DONT_CARE
-            VK_IMAGE_LAYOUT_UNDEFINED
-            VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+   (define make-renderpass (format attachment scene)
+      (renderpass
+         (renderpass-description
+            (renderpass-attachment
+               format
+               VK_SAMPLE_COUNT_1_BIT
+               VK_ATTACHMENT_LOAD_OP_CLEAR
+               VK_ATTACHMENT_STORE_OP_STORE
+               VK_ATTACHMENT_LOAD_OP_DONT_CARE
+               VK_ATTACHMENT_STORE_OP_DONT_CARE
+               VK_IMAGE_LAYOUT_UNDEFINED
+               VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
 
-         (renderpass-attachment
-            VK_FORMAT_D32_SFLOAT
-            VK_SAMPLE_COUNT_1_BIT
-            VK_ATTACHMENT_LOAD_OP_CLEAR
-            VK_ATTACHMENT_STORE_OP_STORE
-            VK_ATTACHMENT_LOAD_OP_DONT_CARE
-            VK_ATTACHMENT_STORE_OP_DONT_CARE
-            VK_IMAGE_LAYOUT_UNDEFINED
-            VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+            (renderpass-attachment
+               VK_FORMAT_D32_SFLOAT
+               VK_SAMPLE_COUNT_1_BIT
+               VK_ATTACHMENT_LOAD_OP_CLEAR
+               VK_ATTACHMENT_STORE_OP_STORE
+               VK_ATTACHMENT_LOAD_OP_DONT_CARE
+               VK_ATTACHMENT_STORE_OP_DONT_CARE
+               VK_IMAGE_LAYOUT_UNDEFINED
+               VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
 
-         (subpass 
-            (pipeline-bindpoint VK_PIPELINE_BIND_POINT_GRAPHICS)
-            (color-attachment (uint32 0) VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL)
-            (depth-attachment (uint32 1) VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)))
+            (subpass 
+               (pipeline-bindpoint VK_PIPELINE_BIND_POINT_GRAPHICS)
+               (color-attachment (uint32 0) VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL)
+               (depth-attachment (uint32 1) VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)))
 
-      (framebuffer
-         lod-color-attachment
-         (framebuffer-attachment
-            VK_FORMAT_D32_SFLOAT
-            (imageusageflags VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)
-            (imageaspectflags VK_IMAGE_ASPECT_DEPTH_BIT)))
+         (framebuffer
+            attachment
+            (framebuffer-attachment
+               VK_FORMAT_D32_SFLOAT
+               (imageusageflags VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)
+               (imageaspectflags VK_IMAGE_ASPECT_DEPTH_BIT)))
+               
+         scene))
 
-      (shader VK_SHADER_STAGE_FRAGMENT_BIT [[
-         #version 450
+   (define lod-renderpass (make-renderpass
+      VK_FORMAT_R8G8B8A8_UINT
+      lod-color-attachment
+      (group
+         (shader VK_SHADER_STAGE_FRAGMENT_BIT [[
+            #version 450
 
-         layout(location = 0) in vec3 texCoord;
-         layout(location = 0) out uvec4 FragColor;
+            layout(location = 0) in vec3 texCoord;
+            layout(location = 0) out uvec4 FragColor;
 
-         const vec3 textureSize = vec3(4096.0);
-         const vec3 tileSize = vec3(64.0, 32.0, 32.0);
+            const vec3 textureSize = vec3(4096.0);
+            const vec3 tileSize = vec3(64.0, 32.0, 32.0);
 
-         float mipmapLevel(vec3 uv)
-         {
-            vec3 dx = dFdx(uv);
-            vec3 dy = dFdy(uv);
-            float d = max(dot(dx, dx), dot(dy, dy));
-            return 0.5 * log2(d);
-         }
+            float mipmapLevel(vec3 uv)
+            {
+               vec3 dx = dFdx(uv);
+               vec3 dy = dFdy(uv);
+               float d = max(dot(dx, dx), dot(dy, dy));
+               return 0.5 * log2(d);
+            }
 
-         void main() 
-         {
-            float lod = mipmapLevel(texCoord * textureSize);
+            void main() 
+            {
+               float lod = mipmapLevel(texCoord * textureSize);
 
-            uint mip = uint(lod);
-            mip = uint(max(int(mip) - 3, 0));
-            uvec3 ijk = uvec3(texCoord * (textureSize / tileSize));
+               uint mip = uint(lod);
+               mip = uint(max(int(mip) - 3, 0));
+               uvec3 ijk = uvec3(texCoord * (textureSize / tileSize));
 
-            FragColor = uvec4(ijk.x >> mip, ijk.y >> mip, ijk.z >> mip, mip);
-         }
-      ]])
+               FragColor = uvec4(ijk.x >> mip, ijk.y >> mip, ijk.z >> mip, mip);
+            }
+         ]])
 
-      (indexed-shape 
-         (bufferdata-uint32 0 1 2 2 3 0)
-         (bufferdata-float 0 0 0.5  1 0 0.5  1 1 0.5  0 1 0.5))
-
-      (offscreen-image lod-color-attachment)))
-
-
-   (define main-renderpass (renderpass
-      (renderpass-description
-         (renderpass-attachment
-            VK_FORMAT_B8G8R8A8_UNORM
-            VK_SAMPLE_COUNT_1_BIT
-            VK_ATTACHMENT_LOAD_OP_CLEAR
-            VK_ATTACHMENT_STORE_OP_STORE
-            VK_ATTACHMENT_LOAD_OP_DONT_CARE
-            VK_ATTACHMENT_STORE_OP_DONT_CARE
-            VK_IMAGE_LAYOUT_UNDEFINED
-            VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
-
-         (renderpass-attachment
-            VK_FORMAT_D32_SFLOAT
-            VK_SAMPLE_COUNT_1_BIT
-            VK_ATTACHMENT_LOAD_OP_CLEAR
-            VK_ATTACHMENT_STORE_OP_STORE
-            VK_ATTACHMENT_LOAD_OP_DONT_CARE
-            VK_ATTACHMENT_STORE_OP_DONT_CARE
-            VK_IMAGE_LAYOUT_UNDEFINED
-            VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
-
-         (subpass 
-            (pipeline-bindpoint VK_PIPELINE_BIND_POINT_GRAPHICS)
-            (color-attachment (uint32 0) VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL)
-            (depth-attachment (uint32 1) VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)))
-
-      (framebuffer
-         main-color-attachment
-         (framebuffer-attachment
-            VK_FORMAT_D32_SFLOAT
-            (imageusageflags VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)
-            (imageaspectflags VK_IMAGE_ASPECT_DEPTH_BIT)))
-
-      (shader VK_SHADER_STAGE_FRAGMENT_BIT [[
-         #version 450
-
-         layout(binding = 1) uniform sampler3D Texture;
-         layout(location = 0) in vec3 texCoord;
-         layout(location = 0) out vec4 FragColor;
-
-         layout(std140, binding = 2) uniform Test {
-            float textureOffset;
-         };
-
-         void main() {
-            vec3 tc = texCoord;
-            // tc.z += textureOffset;
-            tc.z += 0.01;
-            float r = texture(Texture, tc).r;
-            FragColor = vec4(r, r, r, 1.0);
-         }
-         
-      ]])
-
-      (sparse-texture2d "world/world.ktx")
-
-      (textureoffset)
-      (descriptorsetlayoutbinding 
-         (uint32 2) 
-         VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER 
-         VK_SHADER_STAGE_FRAGMENT_BIT)
+         (slice 0.5)
+            
+         (offscreen-image lod-color-attachment))))
 
 
-      (indexed-shape 
-         (bufferdata-uint32 0 1 2 2 3 0)
-         (bufferdata-float 0 0 0.5  1 0 0.5  1 1 0.5  0 1 0.5))))
+   (define main-renderpass (make-renderpass
+      VK_FORMAT_B8G8R8A8_UNORM
+      main-color-attachment
+      (group
+         (shader VK_SHADER_STAGE_FRAGMENT_BIT [[
+            #version 450
+
+            layout(binding = 1) uniform sampler3D Texture;
+            layout(location = 0) in vec3 texCoord;
+            layout(location = 0) out vec4 FragColor;
+
+            layout(std140, binding = 2) uniform Test {
+               float textureOffset;
+            };
+
+            void main() {
+               vec3 tc = texCoord;
+               tc.z += textureOffset;
+               tc.z += 0.01;
+               float r = texture(Texture, tc).r;
+               FragColor = vec4(r, r, r, 1.0);
+            }
+            
+         ]])
+
+         (sparse-texture "world/world.ktx")
+
+         (textureoffset)
+         (descriptorsetlayoutbinding 
+            (uint32 2) 
+            VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER 
+            VK_SHADER_STAGE_FRAGMENT_BIT)
+
+         (slice 0.5))))
 
    (define scene 
       (group
          (viewmatrix 
             .5 .5 2 
-            .4 .4 0 
+            0 0 0 
             0 1 0)
 
          (projmatrix 1000 0.001 1.0 0.7)
