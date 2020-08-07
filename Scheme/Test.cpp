fb85e3895446914ffbe54107a46d2d6d2cd7b860
Frode Oijord <FOeijord@slb.com>
2020-08-07 11:13:42 +0200

    rename ASSERT to TEST

--- Scheme/Test.cpp
+++ Scheme/Test.cpp
@@ -17,7 +17,7 @@ std::string repl(std::string input)
 }
 
 
-#define ASSERT(__exp__, __ev__)											\
+#define TEST(__exp__, __ev__)											\
 	std::string __v__ = repl(__exp__);									\
 	std::cout << __exp__ << " => " << __v__ << " ";						\
 	bool passed = (__ev__ == __v__);									\
@@ -27,27 +27,29 @@ std::string repl(std::string input)
 
 std::vector<std::function<bool()>> tests
 {
-	[] { ASSERT("(+ 2 2)", "4"); },
-	[] { ASSERT("(+ (* 2 100) (* 1 10))", "210"); },
-	[] { ASSERT("(if (> 6 5) (+ 1 1) (+ 2 2))", "2"); },
-	[] { ASSERT("(if (< 6 5) (+ 1 1) (+ 2 2))", "4"); },
-	[] { ASSERT("(define x 3)", "3"); },
-	[] { ASSERT("x", "3"); },
-	[] { ASSERT("(+ x x)", "6"); },
-	[] { ASSERT("((lambda (x) (+ x x)) 5)", "10"); },
-	[] { ASSERT("(define twice (lambda (x) (* 2 x)))", "struct scm::Function"); },
-	[] { ASSERT("(twice 5)", "10"); },
-	[] { ASSERT("(define compose (lambda (f g) (lambda (x) (f (g x)))))", "struct scm::Function"); },
-	[] { ASSERT("((compose list twice) 5)", "(10)"); },
-	[] { ASSERT("(define repeat (lambda (f) (compose f f)))", "struct scm::Function"); },
-	[] { ASSERT("((repeat twice) 5)", "20"); },
-	[] { ASSERT("((repeat (repeat twice)) 5)", "80"); },
-	[] { ASSERT("(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))", "struct scm::Function"); },
-	[] { ASSERT("(fact 3)", "6"); },
-	[] { ASSERT("(fact 50)", "3.04141e+64"); },
-	[] { ASSERT("(define abs (lambda (n) ((if (> n 0) + -) 0 n)))", "struct scm::Function"); },
-	[] { ASSERT("(list (abs -3) (abs 0) (abs 3))", "(3 0 3)"); },
-	[] { 
+	[] { TEST("(quote ())", "()"); },
+	[] { TEST("(quote (testing 1 (2) -3.14e+159))", "(testing 1 (2) -3.14e+159)"); },
+	[] { TEST("(+ 2 2)", "4"); },
+	[] { TEST("(+ (* 2 100) (* 1 10))", "210"); },
+	[] { TEST("(if (> 6 5) (+ 1 1) (+ 2 2))", "2"); },
+	[] { TEST("(if (< 6 5) (+ 1 1) (+ 2 2))", "4"); },
+	[] { TEST("(define x 3)", "3"); },
+	[] { TEST("x", "3"); },
+	[] { TEST("(+ x x)", "6"); },
+	[] { TEST("((lambda (x) (+ x x)) 5)", "10"); },
+	[] { TEST("(define twice (lambda (x) (* 2 x)))", "struct scm::Function"); },
+	[] { TEST("(twice 5)", "10"); },
+	[] { TEST("(define compose (lambda (f g) (lambda (x) (f (g x)))))", "struct scm::Function"); },
+	[] { TEST("((compose list twice) 5)", "(10)"); },
+	[] { TEST("(define repeat (lambda (f) (compose f f)))", "struct scm::Function"); },
+	[] { TEST("((repeat twice) 5)", "20"); },
+	[] { TEST("((repeat (repeat twice)) 5)", "80"); },
+	[] { TEST("(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))", "struct scm::Function"); },
+	[] { TEST("(fact 3)", "6"); },
+	[] { TEST("(fact 50)", "3.04141e+64"); },
+	[] { TEST("(define abs (lambda (n) ((if (> n 0) + -) 0 n)))", "struct scm::Function"); },
+	[] { TEST("(list (abs -3) (abs 0) (abs 3))", "(3 0 3)"); },
+	[] {
 		std::string input = R"(
 			(define combine (lambda (f)
 				(lambda (x y)
@@ -55,9 +57,8 @@ std::vector<std::function<bool()>> tests
 						(f (list (car x) (car y))
 							((combine f) (cdr x) (cdr y)))))))
 						)";
-		ASSERT(input, "struct scm::Function");
+		TEST(input, "struct scm::Function");
 	},
-
 };
 
 
@@ -66,7 +67,7 @@ int main(int, char* [])
 	std::vector<bool> results(tests.size());
 	std::transform(tests.begin(), tests.end(), results.begin(), [](std::function<bool()> test) {
 		return test();
-	});
+		});
 
 	size_t n_exec = results.size();
 	size_t n_fail = std::count(results.begin(), results.end(), false);
